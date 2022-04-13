352c8d40e7cf33445a033993351b84383075ce56
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-14 03:04:06 +0600

    add reservation form

--- src/components/RoomDetail/RoomDetail.js
+++ src/components/RoomDetail/RoomDetail.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import "./RoomDetail.css";
 import picture from "../../images/room-1.jpg";
+import { Form } from "react-bootstrap";
 
 const RoomDetail = () => {
   const { roomsId } = useParams();
   return (
     <div className="container my-3 my-lg-5">
       <div className="row row-cols-1 row-cols-lg-2">
-        <div className="col">
+        <div className="col col-lg-8">
           <div className="room-banner parent">
             <div className="child">
               <img className="img-fluid" src={picture} alt="" />
@@ -27,8 +28,62 @@ const RoomDetail = () => {
             }}
           ></div>
         </div>
-        <div className="col">
-          <Link to="/room_booking">Room Booking</Link>
+        <div className="col col-lg-4">
+          <div className="reservationForm px-4">
+            <div className="row">
+              <div className="col">
+                <div className="row mt-3">
+                  <header className="mb-2">
+                    <h3 className="text-white">Your Reservation</h3>
+                  </header>
+                  <hr />
+                  <div className="col text-start">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label style={{ color: "#fff" }}>
+                          Check in
+                        </Form.Label>
+                        <Form.Control id="date" type="date" name="date" />
+                      </Form.Group>
+                    </Form>
+                  </div>
+                  <div className="col text-start">
+                    <span style={{ color: "#fff" }}>Rooms</span>
+                    <div className="custom-select">
+                      <select className="selectOption">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="col text-start">
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label style={{ color: "#fff" }}>Check Out</Form.Label>
+                    <Form.Control id="date" type="date" name="date" />
+                  </Form.Group>
+                </Form>
+              </div>
+              <div className="col"></div>
+            </div>
+            <div className="row pt-5">
+              <div className="col">
+                <Link id="roomBooking" to="/room_booking">
+                  Room Booking
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
